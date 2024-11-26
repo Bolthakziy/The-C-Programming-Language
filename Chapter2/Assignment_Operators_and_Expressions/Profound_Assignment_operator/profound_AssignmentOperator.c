5fa8b4804f951d092234d82f8b40c1b567747dfd
Alec K <boltakziy@gmail.com>
2024-11-27 02:28:29 +0900

    Assignment Operators and Expressions

---
+++ Chapter2/Assignment_Operators_and_Expressions/Profound_Assignment_operator/profound_AssignmentOperator.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	int x, y;
+
+	printf("Input your 2 numbers : ");
+	scanf("%d %d", &x, &y);
+	printf("The expression \'x *= y + 1\' will be done.\n\n\n\n\n\n\n");
+	printf("%d\n", x *= y + 1);
+
+	return 0;
+}
