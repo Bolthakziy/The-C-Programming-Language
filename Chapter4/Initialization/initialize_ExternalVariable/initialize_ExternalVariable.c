98ba8bacc49875e4a47cac332aeabb35d05fc6a8
Alec K <boltakziy@gmail.com>
2025-04-05 23:23:16 +0900

    Initialization

---
+++ Chapter4/Initialization/initialize_ExternalVariable/initialize_ExternalVariable.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int i = 100;
+char ch = '\"';
+long l = 1000L * 300L * 2400L;
+
+int main()
+{
+	printf("i : %d\n", i);
+	printf("ch : %c\n", ch);
+	printf("l : %ld\n", l);
+
+	return 0;
+}
