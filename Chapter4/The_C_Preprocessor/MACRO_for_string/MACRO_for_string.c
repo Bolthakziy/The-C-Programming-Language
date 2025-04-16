b0f241801049df2ace5402ae3147582384bdb568
Alec K <boltakziy@gmail.com>
2025-04-16 21:33:09 +0900

    The C Preprocessor

---
+++ Chapter4/The_C_Preprocessor/MACRO_for_string/MACRO_for_string.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#define  EN   1
+#define  TO   2
+#define  TRE  3
+
+int main()
+{
+        printf("Danish language \'EN\' means %d\n", EN);
+        printf("Danish language \'TO\' means %d\n", TO);
+        printf("Danish language \'TRE\' means %d\n", TRE);
+
+        return 0;
+}
