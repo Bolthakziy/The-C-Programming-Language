13bec35bdd39eb364ed9d66940a16667bc536bc8
Alec K <boltakziy@gmail.com>
2025-05-14 01:41:34 +0900

    Pointers and Arrays

---
+++ Chapter5/5_2_Pointers_and_Function_Arguments/getintTreatingZero/getintTreatingZero.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <ctype.h>
+
+#define  BUFSIZE  100
+
+char buf[BUFSIZE];
+int bufp = 0;
+
+char getch(void);
+void ungetch(int c);
+int getint(char *pn);
+
+int main()
+{
+	printf("Enter your number string. : ");
+	scanf("%s", buf);
+	printf("Your number string is converted a decimal number like below!\n");
+	printf("%d\n", getint(buf));
+
+	return 0;
+}
+
+char getch()
+{
+	return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c)
+{
+	if (bufp >= BUFSIZE) {
+		printf("ungetch : too many characters\n");
+	} else {
+		buf[bufp++] = c;
+	}
+}
+
+int getint(char *pn)
+{
+	int c, d, sign;
+
+	while (isspace(c = getch()));
+
+	if (!isdigit(c) && c != EOF && c != '+' && c != '-') {
+		ungetch(c);
+
+		return 0;
+	} else {
+	
+	}
+
+	sign = (c == '-') ? -1 : 1;
+
+	if (c == '+' || c == '-') {
+		d = c;
+		
+		if (!isdigit(c = getch())) {
+			if (c != EOF) {
+				ungetch(c);
+			} else {
+
+			}
+
+			ungetch(d);
+
+			return d;
+		} else {
+
+		}
+	} else {
+	
+	}
+
+	for (*pn = 0; isdigit(c); c = getch()) {
+		*pn = 10 * *pn + (c - '0');
+	}
+
+	*pn *= sign;
+
+	if (c != EOF) {
+		ungetch(c);
+	} else {
+	
+	}
+
+	return c;
+}
