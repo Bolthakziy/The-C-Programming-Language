26fc587c9a06469e87876bcdfde08b6c3c333391
Alec K <boltakziy@gmail.com>
2025-05-31 23:07:21 +0900

    Pointers and Arrays

---
+++ Chapter5/5_6_Pointer_Arrays_Pointers_to_Pointers/rewrite_readlines/rewrite_readlines.c
@@ -0,0 +1,118 @@
+#include <stdio.h>
+#include <string.h>
+
+#define  MAXLINES   5000
+#define  ALLOCSIZE  10000
+#define  MAXLEN     1000
+#define  MAXSTOR    5000
+
+char *lineptr[MAXLINES];
+char *linestor;
+
+int mygetline(char *s, int lim);
+int readlines(char *lineptr[], char *linestor, int maxlines);
+void writelines(char *lineptr[], int nlines);
+void swap(char *v[], int i, int j);
+void qsort(char *v[], int left, int right);
+
+int main()
+{
+	int nlines;
+
+	if ((nlines = readlines(lineptr, linestor, MAXLINES)) >= 0) {
+		qsort(lineptr, 0, nlines - 1);
+		writelines(lineptr, nlines);
+
+		return 0;
+	} else {
+		printf("Error : Input too big to sort.\n");
+
+		return 1;
+	}
+}
+
+int mygetline(char *s, int lim)
+{
+	int c, i = 0;
+
+	while ((--lim > 0) && (((c = getchar()) != EOF) && (c != '\n'))) {
+		*(s + i) = c;
+		i++;
+	}
+
+	if (c == '\n') {
+		*(s + i) = c;
+		i++;
+	} else {
+	
+	}
+
+	*(s + i) = '\0';
+
+	return i;
+}
+
+int readlines(char *lineptr[], char *linestor, int maxlines)
+{
+	int len, nlines = 0;
+	char *p = linestor, *linestop = linestor + MAXSTOR;
+	char line[MAXLEN];
+
+	while ((len = mygetline(line, MAXLEN)) > 0) {
+		if ((nlines >= maxlines) || ((p + len > linestop))) {
+			return -1;
+		} else {
+			line[len - 1] = '\0';
+			strcpy(p, line);
+			lineptr[nlines++] = p;
+			p += len;
+		}
+	}
+
+	return nlines;
+}
+
+void writelines(char *lineptr[], int nlines)
+{
+	int i;
+
+	for (i = 0; i < nlines; i++) {
+		printf("%s\n", lineptr[i]);
+	}
+}
+
+void swap(char *v[], int i, int j)
+{
+	char *temp;
+
+	temp = v[i];
+	v[i] = v[j];
+	v[j] = temp;
+}
+
+void qsort(char *v[], int left, int right)
+{
+	int i, last;
+	void swap(char *v[], int i, int j);
+
+	if (left >= right) {
+		return;
+	} else {
+	
+	}
+
+	swap(v, left, (left + right) / 2);
+	last = left;
+
+	for (i = left + 1; i <= right; i++) {
+		if (strcmp(v[i], v[left]) < 0) {
+			swap(v, ++last, i);
+		} else {
+		
+		}
+	}
+
+	swap(v, left, last);
+	qsort(v, left, last - 1);
+	qsort(v, last + 1, right);
+}
