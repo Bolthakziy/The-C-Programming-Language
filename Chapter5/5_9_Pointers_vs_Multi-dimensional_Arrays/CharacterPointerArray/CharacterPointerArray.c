c3e1e20945310abefd54351e181f21f288487430
Alec K <boltakziy@gmail.com>
2025-06-08 22:02:36 +0900

    Pointers and Arrays

---
+++ Chapter5/5_9_Pointers_vs_Multi-dimensional_Arrays/CharacterPointerArray/CharacterPointerArray.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+char *zwierze[7] = {"Mysz", "Zajac", "Kura", "Kon", "Pies", "Kot", "Tygrys"},
+     *liczby[7] = {"Pierwszy", "Drugi", "Trzeci", "Czwarty", "Piaty", "Szosty", "Siodmy"};
+
+int main()
+{
+	int i;
+
+	for (i = 0; i < 7; ++i) {
+		printf("%s zwierze : %s\n", liczby[i], zwierze[i]);
+	}
+
+	return 0;
+}
