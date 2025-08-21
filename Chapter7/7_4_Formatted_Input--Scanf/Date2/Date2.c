cb2e1b897fc1c6335de8f9e9fed3c7c854ea2cbd
Alec K <boltakziy@gmail.com>
2025-08-21 12:03:46 +0900

    Input and Output

---
+++ Chapter7/7_4_Formatted_Input--Scanf/Date2/Date2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+	int day, month, year;
+
+	scanf("%d/%d/%d", &day, &month, &year);
+	printf("Year : %d\tMonth : %d\tDay : %d\n", year, month, day);
+
+	return 0;
+}
