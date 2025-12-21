1d754d4bd22734977e0527c0a6fd1b2e38c07a54
Alek K <boltakziy@gmail.com>
2025-12-21 14:25:14 +0900

    The UNIX System Interface

---
+++ Chapter8/8_7_Example-A_Storage_Allocator/myCalloc/myCalloc.c
@@ -0,0 +1,165 @@
+#include <stdio.h>
+#include <unistd.h>
+
+#define  NALLOC  1024
+
+typedef long Align;
+
+union header
+{
+	struct
+	{
+		union header *ptr;
+		unsigned size;
+	} s;
+
+	Align x;
+};
+
+typedef union header Header;
+
+static Header base;
+static Header *freep = NULL;
+
+void myFree(void *ap);
+static Header *morecore(unsigned nu);
+void *myMalloc(unsigned nbytes);
+void *myCalloc(unsigned n, unsigned size);
+
+int main()
+{
+	int size, i;
+	int *array;
+
+	printf("Enter the size of your integer array! : ");
+	scanf("%d", &size);
+	array = (int*)myCalloc(size, sizeof(int));
+
+	printf("The values in your array!\n\n\n");
+
+	for (i = 0; i < size; i++) {
+		printf("%d\n", *(array + i));
+	}
+
+	myFree(array);
+
+	return 0;
+}
+
+void myFree(void *ap)
+{
+	Header *bp, *p;
+
+	bp = (Header*)ap - 1;
+
+	for (p = freep; !((bp > p) && (bp < p->s.ptr)); p = p->s.ptr) {
+		if ((p >= p->s.ptr) && ((bp > p) || (bp < p->s.ptr))) {
+			break;
+		} else {
+
+		}
+	}
+
+	if ((bp + bp->s.size) == p->s.ptr) {
+		bp->s.size += p->s.ptr->s.size;
+		bp->s.ptr = p->s.ptr->s.ptr;
+	} else {
+		bp->s.ptr = p->s.ptr;
+	}
+
+	if ((p + p->s.size) == bp) {
+		p->s.size += bp->s.size;
+		p->s.ptr = bp->s.ptr;
+	} else {
+		p->s.ptr = bp;
+	}
+
+	freep = p;
+}
+
+static Header *morecore(unsigned nu)
+{
+	char *cp;
+	Header *up;
+
+	if (nu < NALLOC) {
+		nu = NALLOC;
+	} else {
+
+	}
+
+	cp = sbrk(nu * sizeof(Header));
+
+	if (cp == (char*)-1) {
+		return NULL;
+	} else {
+
+	}
+
+	up = (Header*)cp;
+	up->s.size = nu;
+	myFree((void*)(up + 1));
+
+	return freep;
+}
+
+void *myMalloc(unsigned nbytes)
+{
+	Header *p, *prevp;
+	unsigned nunits;
+
+	nunits = (nbytes + sizeof(Header) - 1) / sizeof(Header) + 1;
+
+	if ((prevp = freep) == NULL) {
+		base.s.ptr = freep = prevp = &base;
+		base.s.size = 0;
+	} else {
+
+	}
+
+	for (p = prevp->s.ptr;;prevp = p, p = p->s.ptr) {
+		if (p->s.size >= nunits) {
+			if (p->s.size == nunits) {
+				prevp->s.ptr = p->s.ptr;
+			} else {
+				p->s.size -= nunits;
+				p += p->s.size;
+				p->s.size = nunits;
+			}
+
+			freep = prevp;
+
+			return (void*)(p + 1);
+		} else {
+
+		}
+
+		if (p == freep) {
+			if ((p = morecore(nunits)) == NULL) {
+				return NULL;
+			} else {
+
+			}
+		} else {
+
+		}
+	}
+}
+
+void *myCalloc(unsigned n, unsigned size)
+{
+	unsigned i, nb;
+	char *p, *q;
+
+	nb = n * size;
+
+	if ((p = q = myMalloc(nb)) != NULL) {
+		for (i = 0; i < nb; i++) {
+			*p++ = 0;
+		}
+	} else {
+
+	}
+
+	return q;
+}
