9e7c154468398cc0ae9baf16ad46f38d26ae5eba
Alek K <boltakziy@gmail.com>
2026-09-06 22:09:09 +0900

    Formatted Output

---
+++ APPENDIX_B/B1/B1.2/Function_printf/Function_printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("Devils must encounter the demons which are much strongrt than devils themselves.\n");
+	printf("Rabbits must hide themselves from devils, demons and protect themselves!\n");
+
+	return 0;
+}
